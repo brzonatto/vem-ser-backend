116209aee9f9bff18de3308b8545296f7378d47c
Brunno Zonatto <brzonatto@gmail.com>
2021-10-05 18:56:35 -0300

    add SOUT

--- Lista1/src/com/dbc/Main.java
+++ Lista1/src/com/dbc/Main.java
@@ -3,6 +3,7 @@ package com.dbc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        System.out.println("Lista de Exercicios 1");
     }
 }
