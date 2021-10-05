b0c5eff501d462f1cd6787ce4a39b5c7a428fc2a
Brunno Zonatto <brzonatto@gmail.com>
2021-10-05 18:55:56 -0300

    add Exercicio 1

---
+++ Lista1/src/com/dbc/SolicitandoScanner.java
@@ -0,0 +1,23 @@
+package com.dbc;
+
+import java.util.Scanner;
+
+public class SolicitandoScanner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scan.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Digite sua cidade: ");
+        String cidade = scan.nextLine();
+        System.out.println("Digite seu estado: ");
+        String estado = scan.nextLine();
+
+        System.out.println("Olá seu nome é " + nome + ", você tem " + idade + " anos, é da cidade de " + cidade + ", situada " +
+                "no estado de " + estado +".");
+
+    }
+}
