be208abaca61f054b53be6a09a4bf0c6fdb78fc9
Brunno Zonatto <brzonatto@gmail.com>
2021-10-08 01:52:55 -0300

    MODIFIED CONTATO CLASS

--- Lista3/src/com/dbc/Contato.java
+++ Lista3/src/com/dbc/Contato.java
@@ -2,16 +2,29 @@ package com.dbc;
 
 public class Contato {
     public String descricao;
-    public String telefonoe;
+    public String telefone;
     public Integer tipo;
 
-    public Contato(String descricao, String telefonoe, Integer tipo) {
+    public Contato(String descricao, String telefone, Integer tipo) {
         this.descricao = descricao;
-        this.telefonoe = telefonoe;
+        this.telefone = telefone;
         this.tipo = tipo;
     }
 
     public void imprimirContato() {
+        String tipoNome = "";
+        if (this.tipo == 1) {
+            tipoNome = "Residêncial";
+        } else if (this.tipo == 2) {
+            tipoNome = "Comercial";
+        }
+        System.out.println("Contato Tipo: " + tipoNome + "\n" + this.toString());
+    }
 
+    @Override
+    public String toString() {
+        return this.telefone + " (" + this.descricao + ")";
     }
 }
+
+
