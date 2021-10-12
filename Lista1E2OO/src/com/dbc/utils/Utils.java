f449cf3d89166e34b325a57c1252c6b9b404d854
Brunno Zonatto <brzonatto@gmail.com>
2021-10-12 20:25:15 -0300

    add UTILS

---
+++ Lista1E2OO/src/com/dbc/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.dbc.utils;
+
+import java.text.DecimalFormat;
+
+public class Utils {
+    final static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,##0.00");
+
+    public static String formatarDouble(double valor){
+        return DECIMAL_FORMAT.format(valor);
+    }
+}
