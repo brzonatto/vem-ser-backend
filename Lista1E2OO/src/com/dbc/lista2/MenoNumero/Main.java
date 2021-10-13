f75f7c5a8eb8cbc2d8424c08c7b5daee51e221a0
Brunno Zonatto <brzonatto@gmail.com>
2021-10-13 10:48:58 -0300

    add EX4 MENORNUMERO

---
+++ Lista1E2OO/src/com/dbc/lista2/MenoNumero/Main.java
@@ -0,0 +1,9 @@
+package com.dbc.lista2.MenoNumero;
+
+public class Main {
+    public static void main(String[] args) {
+        Numeros n1 = new Numeros(3);
+        n1.preencherVetor();
+        n1.encontrarMenorNumero();
+    }
+}
---
+++ Lista1E2OO/src/com/dbc/lista2/MenoNumero/Numeros.java
@@ -0,0 +1,45 @@
+package com.dbc.lista2.MenoNumero;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Numeros {
+    private Integer[] numeros;
+
+    public Numeros() {
+    }
+
+    public Numeros(Integer tamanho) {
+        this.numeros = new Integer[tamanho];
+    }
+
+    public Integer[] getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(Integer[] numeros) {
+        this.numeros = numeros;
+    }
+
+    public void preencherVetor() {
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);
+
+        for (int i = 0; i < this.numeros.length; i++) {
+            System.out.println("Digite o " + (i+1) + "° número: ");
+            this.numeros[i] = scan.nextInt();
+        }
+    }
+
+    public void encontrarMenorNumero() {
+        Integer menor = numeros[0];
+        Integer posicao = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+                posicao = i;
+            }
+        }
+        System.out.println("O menor número é " + menor + " no indice " + posicao);
+    }
+}
