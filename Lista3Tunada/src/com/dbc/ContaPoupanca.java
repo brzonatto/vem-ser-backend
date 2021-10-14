4c2e3362927114cada69d36796b09fd058dc94e1
Brunno Zonatto <brzonatto@gmail.com>
2021-10-14 00:12:54 -0300

    IMPLEMENTANDO LISTA3TUNADA CLASSE CONTAPOUPANCA

--- Lista3Tunada/src/com/dbc/ContaPoupanca.java
+++ Lista3Tunada/src/com/dbc/ContaPoupanca.java
@@ -1,5 +1,7 @@
 package com.dbc;
 
+import com.dbc.utils.Utils;
+
 public class ContaPoupanca extends Conta implements Impressao {
     static final Double JUROS_MENSAL = 1.01;
 
@@ -8,11 +10,18 @@ public class ContaPoupanca extends Conta implements Impressao {
     }
 
     public void creditarTaxa() {
-
+        super.setSaldo(super.getSaldo() * JUROS_MENSAL);
     }
 
     @Override
     public void imprimir() {
+        System.out.println("Proprietário da conta poupança: " + super.getCliente().getNome());
+        System.out.println(this);
+    }
 
+    @Override
+    public String toString() {
+        return "Número da conta: " + super.getNumeroConta() + " | Número da Agência: " + super.getAgencia()
+                + " | Saldo: " + Utils.formatarDouble(super.getSaldo());
     }
 }
