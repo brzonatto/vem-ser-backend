9506f10104668cafb12bb328b64744b646f07848
Brunno Zonatto <brzonatto@gmail.com>
2021-10-14 00:59:59 -0300

    ADD LISTA3TUNADA CLASSE CONTAPAGAMENTO

---
+++ Lista3Tunada/src/com/dbc/ContaPagamento.java
@@ -0,0 +1,34 @@
+package com.dbc;
+
+import com.dbc.utils.Utils;
+
+public class ContaPagamento extends Conta implements Impressao {
+    final static Double TAXA_SAQUE = 4.25;
+
+    public ContaPagamento(Cliente cliente, String numeroConta, String agencia, Double saldo) {
+        super(cliente, numeroConta, agencia, saldo);
+    }
+
+    @Override
+    public Boolean sacar(Double valor) {
+        if ((valor + TAXA_SAQUE) < this.getSaldo()) {
+            this.setSaldo(this.getSaldo() - TAXA_SAQUE);
+            return super.sacar(valor);
+        }
+        System.out.println("Saldo insuficiente! É necessario o pagamento da taxa de " + Utils.formatarDouble(TAXA_SAQUE)
+                + " para realizar uma transação de saque ou tranferencia.");
+        return false;
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Proprietário da conta pagamento: " + super.getCliente().getNome());
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Número da conta: " + super.getNumeroConta() + " | Número da Agência: " + super.getAgencia()
+                + " | Saldo: " + Utils.formatarDouble(super.getSaldo());
+    }
+}
