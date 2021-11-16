b82d738ede58ae0235df9a58f1d26a79aef85c54
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-11-16 17:52:24 -0300

    homework

--- modulo3_3/pessoaapi/src/main/java/com/dbc/pessoaapi/security/WebSecurityConfig.java
+++ modulo3_3/pessoaapi/src/main/java/com/dbc/pessoaapi/security/WebSecurityConfig.java
@@ -1,21 +1,33 @@
 package com.dbc.pessoaapi.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenService tokenService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable().and().cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/auth").permitAll()
+//                .antMatchers("/contato").permitAll()
+
+                .anyRequest().authenticated()
+
+                //filtro de autenticação...
+                .and().addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Override
@@ -24,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-ui/**",
                 "/v2/api-docs",
                 "/swagger-ui.html",
-                "/swagger-resources/**");
+                "/swagger-resources/**"
+                );
     }
 }
