93b23fdc0b654a5c9ce54430fe83d74df8df06a3
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-11-29 22:28:31 -0300

    ADD HOMEWORK

---
+++ modulo6_1/consumer/src/main/java/com/dbc/consumer/config/KafkaProducerConfig.java
@@ -0,0 +1,34 @@
+package com.dbc.consumer.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${kafka.bootstrap-server}")
+    private String bootstrapServer;
+
+    private <T> ProducerFactory<String, T> genericProducerFactory() {
+        Map<String, Object> propriedades = new HashMap<>();
+        propriedades.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        propriedades.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        propriedades.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(propriedades);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> stringKafkaTemplate() {
+        return new KafkaTemplate<>(genericProducerFactory());
+    }
+}
